968da7d10c18ec8c4a5f8e6062dcf54fab5fd3c0
mohitthakur007 <41528050+mohitthakur007@users.noreply.github.com>
2019-12-23 05:30:40 +0530

    Update and rename Longest common Subsequence Incomplete to Longest common Subsequence.cpp

--- Longest common Subsequence.cpp
+++ Longest common Subsequence.cpp
@@ -1,4 +1,4 @@
-//1143. Longest Common Subsequence
+//1143. Longest Common Subsequence Bottom-up
 class Solution {
 public:
     int lcs(string text1, string text2,int m,int n,int **dp){
@@ -42,3 +42,44 @@ public:
        
        
 };
+
+//TOP-DOWN
+class Solution {
+public:
+    int lcs(string text1, string text2,int m,int n,int **dp){
+       for(int i=1;i<=m;i++){
+           for(int j=1;j<=n;j++){
+               if(text1[m-i]==text2[n-j])
+                dp[i][j]=1+dp[i-1][j-1];
+                else 
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+           }
+       }
+        return dp[m][n];
+        }
+     
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.length();
+        int n=text2.length();
+        int **dp=new int*[m+1];
+        for(int i=0;i<m+1;i++){
+            dp[i]=new int[n+1];
+        }
+    
+        
+    
+    for(int i=0;i<m+1;i++){
+        for(int j=0;j<n+1;j++){
+            dp[i][j]=0;
+        }
+    }
+       int t= lcs(text1,text2,m,n,dp);
+        return t;
+    
+    }
+       
+       
+};
+
+
+
